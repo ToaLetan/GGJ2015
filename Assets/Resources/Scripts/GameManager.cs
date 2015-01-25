27b98359dc487b4442bcf3730cc2f6a8ce617b9f
Tyler Norman <letan881@hotmail.com>
2015-01-25 01:25:02 -0500

    Intro, Wins Display
    Game plays intro before rounds, displays number of won rounds.

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -4,11 +4,33 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour 
 {
-    public const float ROUND_LENGTH = 90; //Rounds are 1 minute 30 seconds.
+    public const float ROUND_LENGTH = 2; //Rounds are 1 minute 30 seconds.
+    public const float NUM_ROUNDS = 3; //Play up to 3 rounds max.
+
+    private const float ICON_OFFSET = 0.25f;
+    private const float INTRO_TIME = 1.0f;
+    private const float SLIDER_MOVE_SPEED = 1.5f;
+
+    private Timer roundIntroTimer = null;
+    private Timer gameTimer = null;
+
+    private GameObject[] playerWinsText = new GameObject[2];
+
+    private GameObject sliderUI = null;
+
+    private Vector3 sliderDestination = Vector3.zero;
+    private Vector3 sliderStartPos = Vector3.zero;
 
     private int[] playerScores = new int[2];
+    private int[] playerWins = new int[2];
 
-    private Timer gameTimer = null;
+    private float sliderAcceleration = 0.0f;
+
+    private int currentRound = 0;
+    private int introPhase = 0; //0 = READY?, 1 = EAT!
+
+    private bool isGamePaused = false;
+    private bool isSliderAtDestination = false;
 
     private InputManager inputManager = null;
 
@@ -22,6 +44,12 @@ public class GameManager : MonoBehaviour
         get { return gameTimer; }
     }
 
+    public bool IsGamePaused
+    {
+        get { return isGamePaused; }
+        set { isGamePaused = value; }
+    }
+
 	// Use this for initialization
 	void Start () 
     {
@@ -30,26 +58,163 @@ public class GameManager : MonoBehaviour
         playerScores[0] = 0;
         playerScores[1] = 0;
 
-        gameTimer = new Timer(ROUND_LENGTH, true); //TODO: MAKE A "ROUND START!" THING BEFORE THIS RUNS
+        playerWins[0] = 0;
+        playerWins[1] = 0;
+
+        playerWinsText[0] = GameObject.Find("Text_Wins_P1");
+        playerWinsText[1] = GameObject.Find("Text_Wins_P2");
+
+        sliderUI = GameObject.Find("UI_Slider");
+        sliderStartPos = sliderUI.transform.position;
+        sliderDestination = new Vector3(-0.74f, 0.092f, 0);
+
+        currentRound = 1; //Obviously we start at the first round.
+
+        gameTimer = new Timer(ROUND_LENGTH); //TODO: MAKE A "ROUND START!" THING BEFORE THIS RUNS
         gameTimer.OnTimerComplete += OnRoundOver;
+
+        roundIntroTimer = new Timer(INTRO_TIME, true);
+        roundIntroTimer.OnTimerComplete += PlayRoundIntro;
+
+        isGamePaused = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        inputManager.Update();
-        gameTimer.Update();
+        if (isGamePaused == false)
+        {
+            inputManager.Update();
+            gameTimer.Update();
+        }
+
+        if (isSliderAtDestination == false)
+            UpdateSliderMovement();
+        else
+            roundIntroTimer.Update();
 	}
 
     public void AdjustScore(int playerNum, int scoreToAdd)
     {
         playerScores[playerNum - 1] += scoreToAdd;
-
-        Debug.Log(playerScores[playerNum - 1]);
     }
 
     private void OnRoundOver()
     {
         //Determine the winner here.
+        if (playerScores[0] > playerScores[1]) //Player 1 wins
+        {
+            playerWins[0]++;
+            DisplayWin(1);
+        }
+        else if (playerScores[0] < playerScores[1]) //Player 2 wins
+        {
+            playerWins[1]++;
+            DisplayWin(2);
+        }
+        else if (playerScores[0] == playerScores[1]) //It's a tie, everyone wins
+        {
+            playerWins[0]++;
+            playerWins[1]++;
+
+            DisplayWin(1);
+            DisplayWin(2);
+        }
+
+        //Reset round score
+        playerScores[0] = 0;
+        playerScores[1] = 0;
+
+        //Prepare the next round if it's not over.
+        if (currentRound > NUM_ROUNDS)
+        {
+            gameTimer.ResetTimer(true); //TODO: NOT START INSTANTLY.
+        }
+
+    }
+
+    private void DisplayWin(int playerNumber)
+    {
+        int playerID = playerNumber - 1;
+
+        Vector3 iconPos = playerWinsText[playerID].transform.position;
+        iconPos.x += ICON_OFFSET / 2 + (ICON_OFFSET * playerWins[playerID]);
+        GameObject winIcon = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Icon_Pizza"), iconPos, Quaternion.identity) as GameObject;
+        winIcon.transform.parent = playerWinsText[playerID].transform;
+    }
+
+    private void StartRound()
+    {
+        gameTimer.StartTimer();
+        isGamePaused = false;
+    }
+
+    private void PlayRoundIntro()
+    {
+        GameObject sliderText = sliderUI.transform.FindChild("Slider_Text").gameObject;
+
+        if (introPhase < 2)
+        {
+            switch (introPhase)
+            {
+                case 0:
+                    sliderText.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/UI/Text_Ready");
+                    break;
+                case 1:
+                    sliderText.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/UI/Text_Eat");
+                    break;
+                default:
+                    break;
+            }
+
+            introPhase++;
+            roundIntroTimer.ResetTimer(true);
+        }
+        else //START THE GAME ALREADY after we move back to the left
+        {
+            roundIntroTimer.OnTimerComplete -= PlayRoundIntro;
+
+            sliderDestination = sliderStartPos;
+            isSliderAtDestination = false;
+        } 
+    }
+
+    private void UpdateSliderMovement()
+    {
+        if (sliderDestination != sliderStartPos)
+        {
+            if (sliderUI.transform.position.x < sliderDestination.x) //Move right
+            {
+                Vector3 newPos = sliderUI.transform.position;
+                sliderAcceleration += 0.1f;
+                newPos.x += sliderAcceleration * SLIDER_MOVE_SPEED * Time.deltaTime;
+
+                sliderUI.transform.position = newPos;
+            }
+            else
+            {
+                isSliderAtDestination = true;
+                sliderAcceleration = 0.0f;
+            }
+        }
+        else
+        {
+            if (sliderUI.transform.position.x > sliderDestination.x) //Move left
+            {
+                Vector3 newPos = sliderUI.transform.position;
+                sliderAcceleration += 0.1f;
+                newPos.x += sliderAcceleration * SLIDER_MOVE_SPEED * Time.deltaTime * -1;
+
+                sliderUI.transform.position = newPos;
+            }
+            else
+            {
+                isSliderAtDestination = true;
+                sliderAcceleration = 0.0f;
+
+                StartRound(); //Now we can actually start, jesus christ dude.
+            }   
+        }
+        
     }
 }
